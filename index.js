7e093b588f54f3fd570da0ec7a71123e329febd9
programeshwar <tiwary.rameshwaram@gmail.com>
2022-02-24 20:37:36 +0530

    usnderstood backtrip `

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-// 
+// // variable initilisation
+// a=10
+
+// // typeof operator return integer type
+// console.log(typeof a)
+// console.log(a)
+
+// // typeof operator return string type
+// a="Hello Js"
+// console.log(typeof a)
+// console.log(a)
+
+// // typeof operator return boolean type
+// a=true
+// console.log(typeof a)
+// console.log(a)
+
+// // typeof operator return object type
+// a=null
+// console.log(typeof a )
+// console.log(a)
+
 // Number
 var num = 10;
 var float = 3.4;
@@ -14,5 +35,12 @@ console.log(f);
 // String
 var str = 'a';
 console.log(typeof str);
-str = "How you doin";
+str = "How you doin ?\nI'm fine how about you?";
 console.log(str);
+
+var str2 = 'Im dump and pathatic What do you think?';
+console.log(str2)
+
+// Output with variable and string using backtrick
+var num = 100;
+console.log(`half of ${num} is ${num/2}`);
